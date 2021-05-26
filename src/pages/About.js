b216ceab066da49c60132d377338667097225fb2
umangsadhwani <umangsadhwani02@gmail.com>
2021-05-27 00:53:39 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p style={{textAlign:"center"}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam culpa tempore sapiente, cum sed autem. Repellat, aliquid, nobis temporibus in quia quo sapiente laudantium error est velit reiciendis officia veniam voluptatum delectus ipsa officiis saepe! Accusantium exercitationem autem neque harum.
+      </p>
+    </section>
+  )
+}
+
+export default About
